c78064308c9c1aa0ae199093853d3c9eab76863a
Cao Ming <cm9999@qq.com>
2020-04-30 19:21:24 +0800

    update run.py

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from model import *
 from torch.utils.data import DataLoader
 import torch.nn
 import torch.optim as optim
+
 def train():
     dataset = LiDARSRDataset()
     loader = DataLoader(dataset, batch_size=16, shuffle=True)    
@@ -17,8 +18,8 @@ def train():
 
     from tensorboardX import SummaryWriter
     writer = SummaryWriter(log_dir='/home/caoming/Projects/lidar_SR_pytorch/logs', comment='SR_torch')
-    total_loss = 0.0
     for epoch in range(100):
+        total_loss = 0.0
         i = 1
         for batch_x, batch_y in loader:
             batch_x = batch_x.to(device)
@@ -28,11 +29,11 @@ def train():
             loss = criterion(batch_y.float(), output)
             loss.backward()
             optimizer.step()
-            total_loss = loss.item()
-            writer.add_scalar('Train/Loss', total_loss, epoch)
-            print('[%d, %d]: %f' % (epoch, i, total_loss))
+            total_loss = total_loss + loss.item()
+            print('[%d, %d]: %f' % (epoch, i, total_loss / i))
             i = i + 1
 
+        writer.add_scalar('Train/Loss', total_loss, epoch)
         torch.save(net.state_dict(), '/home/caoming/Projects/lidar_SR_pytorch/models/sr_' + str(epoch)+'.pkl')
 
 
